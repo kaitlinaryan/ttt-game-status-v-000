7676b8084f0751db567578bd0cc80a4c2f13e8d4
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-17 16:15:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,7 +61,7 @@ def winner(board)
      return nil
    elsif won?(board) || position_1 == "O" && position_2 == "O" && position_3 == "O"
      return "O"
-   elsif won?(board) || position_1 == "X" && position_2 == "X" && position_3 == "X"
+   if won?(board) || position_1 == "X" && position_2 == "X" && position_3 == "X"
      return "X"
    end
 end
