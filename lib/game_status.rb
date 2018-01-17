c47f170e2eb53a400b163da5206ef0d8a1cbd56f
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-17 16:20:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,14 +52,14 @@ end
 def over?(board)
 if draw?(board) || won?(board) || full?(board)
   return true
-end
+  end
 end
 #returns true if board has been won, draw or full
 
 def winner(board)
    if !won?(board)
      return nil
-   elsif won?(board) || position_1 == "O" && position_2 == "O" && position_3 == "O"
+   if won?(board) || position_1 == "O" && position_2 == "O" && position_3 == "O"
      return "O"
    else won?(board) || position_1 == "X" && position_2 == "X" && position_3 == "X"
      return "X"
