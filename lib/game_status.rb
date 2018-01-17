fbd11363d2441ab059b0a53c4126a008009bced1
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-01-17 16:07:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,8 +59,8 @@ end
 def winner(board)
     if won?(board) || position_1 == "X" && position_2 == "X" && position_3 == "X"
      return "X"
-    if won?(board) || position_1 == "O" && position_2 == "O" && position_3 == "O"
+   elsif won?(board) || position_1 == "O" && position_2 == "O" && position_3 == "O"
      return "O"
-   if !won?(board)
+   else !won?(board)
      return nil
    end
